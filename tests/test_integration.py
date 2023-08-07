c9f7af36d65b859c89511a2512faa84aa3abf3eb
plcr9 <paulcooperpc9@gmail.com>
2023-08-07 13:03:12 +0100

    integration test added

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -9,3 +9,13 @@ def test_adds_and_lists_incomplete_tasks():
     tracker.add(task_1)
     tracker.add(task_2)
     assert tracker.list_incomplete() == (task_1, task_2)
+
+'''When multiple tasks added, and one task marked as complete, it does not show up in the incomplete list anymore'''
+def test_marks_tasks_as_complete_and_removes_them_from_incomplete():
+    tracker = TaskList()
+    task_1 = Task("Clear the shed")
+    task_2 = Task("Jump start the car")
+    tracker.add(task_1)
+    tracker.add(task_2)
+    task_1.mark_complete()
+    assert tracker.list_incomplete() == (task_2)
